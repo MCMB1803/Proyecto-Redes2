c5680ec0c6bd3b7ea0ce0009fa0a3474c5066547
IgnacioChava <133459180+IgnacioChava@users.noreply.github.com>
2023-09-09 13:00:15 -0600

    Create Game Component

---
+++ app-redes2/src/ListGames.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+const ListGames = () => {
+
+    
+
+
+    useEffect(() => {
+        
+
+    }, [])
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ListGames;
