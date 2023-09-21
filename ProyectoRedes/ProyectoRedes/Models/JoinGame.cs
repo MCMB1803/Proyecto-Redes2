f1d2a465d93eced04c64f070c82bc001305fb43f
henrymc03 <99292415+henrymc03@users.noreply.github.com>
2023-09-21 10:23:22 -0600

    Update JoinGame.cs

--- ProyectoRedes/ProyectoRedes/Models/JoinGame.cs
+++ ProyectoRedes/ProyectoRedes/Models/JoinGame.cs
@@ -2,6 +2,7 @@
 {
     public class JoinGame
     {
+        //Atributos del join game segun la api
         public string gameId { get; set; }
         public string password { get; set; }
         public string player { get; set; }
